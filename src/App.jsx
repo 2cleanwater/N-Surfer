72884761d4a88b8fd3ec363d35585d183b4d2632
2cleanwater <2cleanwater@naver.com>
2022-11-28 00:01:29 +0900

    feat: add submit function with notion

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
-import './App.css';
+import './App.module.css';
+import {useState} from 'react';
+
 
 function App() {
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
+
+  function submitFormToNotion() {
+    console.log("we in " + name )
+    fetch("http://localhost:3000/submitFormNotion". {
+      method: "post",
+      header:{
+        "Accept": "appclication/json",
+        "Concept-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: name,
+        content: content
+      })
+    }).then(response => response.json())
+    .then(data => {
+      console.log("Succes! ", data);
+    }).catch(error => {
+      console.log("Error ", error)
+    });
+  }
+
   return (
-    <div>
-      hey~
+    <div className='App'>
+      <div>
+        <p>Name</p>
+        <input type="text" id="name" onChange={(e)=> setName(e.target.value)}></input>
+
+        <p>Content</p>
+        <textarea id="content" onChange={(e)=> setContent(e.target.value)} rows={10} cols={25}></textarea>
+
+        <div>
+          <button onClick={submitFormToNotion}>submit</button>
+        </div>
+      </div>
     </div>
   );
 }
