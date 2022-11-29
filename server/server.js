5e370386db4e26fcdb60e8bab231753dd53cc320
2cleanwater <2cleanwater@naver.com>
2022-11-29 21:25:24 +0900

    refactor: change project

--- server/server.js
+++ server/server.js
@@ -57,11 +57,69 @@
 // // app.get('/', function)
 
 
-const express = require("express");
+// const express = require("express");
+// const app = express();
+// const test = require("./Router/test");
+
+// app.use("/api", test);
+
+// const port = 5000;
+// app.listen(port, ()=> console.log(`${port}`));
+
+const express = require('express')
+// const dotenv = require('dotenv').config()
+const {Client} = require('@notionhq/client')
+const cors = require('cors');
+let bodyParser = require('body-parser');
+let jsonParser = bodyParser.json();
+
+
 const app = express();
-const test = require("./Router/test");
+const path = require('../client/src/App');
 
-app.use("/api", test);
+app.use(cors());
+app.use("/api", path);
 
-const port = 5000;
-app.listen(port, ()=> console.log(`${port}`));
+const PORT = 4000;
+const HOST = "localhost";
+
+const client = new Client({
+  auth: process.env.NOTION_TOKEN
+});
+
+const databaseId = process.env.NOTION_DATABASE_ID;
+
+app.post('/submitFormNotion', jsonParser, async (req, res)=>{
+  //req body
+  /*{
+
+  }*/
+  const name = req.body.name;
+
+  try{
+    const response = await notion.pages.create({
+      parent: {database_id: databaseId},
+      properties: {
+        Name: {
+          title: [
+            {
+              text: {
+                content: name
+              }
+            }
+          ]
+        }
+      } 
+    });
+    console.log(response);
+    console.log("SUCCESS");
+  } catch(error) {
+    console.log(error);
+  }
+});
+
+app.listen(PORT, HOST, function(){
+  console.log('listening on ' + HOST + ":" + PORT);
+});
+
+// app.get('/', function)
