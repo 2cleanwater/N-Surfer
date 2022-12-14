d7d2ccf1b394e86b2420db322e7840faa4d40082
2cleanwater <2cleanwater@naver.com>
2022-12-14 22:54:17 +0900

    chore: will change app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,20 +4,20 @@ import Home from "./pages/home/Home";
 import Login from "./components/login/login";
 
 
-// function App({authService}) {
-//   return (
-//     <div>
-//       <BrowserRouter>
-//         <Routes>
-//           <Route exact path="/" element={<Home/>}/>
-//           <Route exact path="/login" element={<Login/>}/>
-//           <Route path="/login" element={<Login authService={authService}/>}/>
-//           <Route path="/profile" element={{/* 프로필관련 */}}/>
-//           <Route paht="/maker"/>
-//         </Routes>
-//       </BrowserRouter>
-//     </div>
-//   )
-// }
+function App({authService}) {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/login" element={<Login/>}/>
+          <Route path="/login" element={<Login authService={authService}/>}/>
+          <Route path="/profile" element={{/* 프로필관련 */}}/>
+          <Route paht="/maker"/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
 
 export default App
