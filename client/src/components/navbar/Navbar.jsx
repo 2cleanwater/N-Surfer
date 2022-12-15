dfea655658ecae622d9eafe72b235f169d7df8d9
2cleanwater <2cleanwater@naver.com>
2022-12-15 23:12:29 +0900

    feat: add navbar's menu & icon

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,39 +1,41 @@
 import React, {memo} from 'react'
-import styles from './header.module.css';
+import { Link } from 'react-router-dom';
+import styles from './Navbar.module.css';
+import { useAuthContext } from '../../context/AuthContext';
+import { login, logout, onUserStateChange } from '../../api/firebase';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-// const Header = memo(({onLogout})=>{
-//   <header className={styles.header}>
-//     {onLogout&&(<button className={styles.logout} onClick={onLogout}>Logout</button>)}
-//     <img className={styles.logo} src="../images/logo.png" alt="logo"/>
-//     <h1 className={styles.title}>N-Surfer</h1>
-//   </header>
-// });
+export default function Navbar() {
+  // const {user, login, logout} = useAuthContext();
+  const [user,setUser] = useState();
 
-function Header({onLogout}) {
+  useEffect(()=>{
+    onUserStateChange((user)=>{
+      setUser(user);
+    });
+  },[])
+  const handelLogin = ()=>{
+    login().then(setUser);
+  }
+  const handelLogout = ()=>{
+    logout().then(setUser);
+  }
   return (
     <header className={styles.header}>
-      <img className={styles.logo} src='/images/logo.png' alt="logo"/>
-      <h1 className={styles.title}>N-Surfer</h1>
-      <ul className={styles.menu}>
-        <li>카드 목록</li>
-        {onLogout?
-        (
-            <li>로그인</li>
-        ):
-        (
-            <li>회원가입</li>
-        )}
-      </ul>
-      {onLogout? 
-        (<button className={styles.logout} onClick={onLogout}>Logout</button>) : 
-        (<button className={styles.logout} onClick={onLogout}>Login</button>)
-      }
-      <img className={styles.profile} src='/images/profile_logo.jpg' alt="profile">
-
-      </img>
+      <Link to='/' className={styles.mainLogo}>
+        <img className={styles.logo} src='/images/N-Surfer_Icon.png' alt="logo"/>
+        <h1 className={styles.title}>N-Surfer</h1>
+      </Link>
+      <div className={styles.menu}>
+        <Link to='/card/list' className={styles.menuItem}>카드목록</Link>
+        {user&&(<Link to='/card' className={styles.menuItem}>카드추가</Link>)}
+        {user? 
+          (<button className={styles.logout} onClick={handelLogout}>Logout</button>) : 
+          (<button className={styles.logout} onClick={handelLogin}>Login</button>)
+        }
+        <Link to='/user/profile'><img className={styles.profile} src='/images/profile_logo.jpg' alt="profile" /></Link>
+      </div>
     </header>
-  )
+  );
 }
-
-
-export default Header
