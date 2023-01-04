684a00f6ccf4ab44a13f954e47e933c5ae2f7924
2cleanwater <2cleanwater@naver.com>
2023-01-04 17:23:04 +0900

    chore: add testcase for kakao login

--- src/components/modalLogin/ModalLogin.jsx
+++ src/components/modalLogin/ModalLogin.jsx
@@ -2,23 +2,30 @@ import React from 'react'
 import styles from './ModalLogin.module.css';
 import { observer } from 'mobx-react';
 import useStore from '../../store/useStore';
+import { Box, useTheme } from '@mui/material';
+
+import { Button } from "@mui/material";
+
+
 
 const ModalLogin = ()=>{
   const {value} = useStore();
+  const theme = useTheme();
+
   return (
     <section className={styles.modalContainer}>
       <div className={styles.modal}>
         <button className={styles.close} onClick={()=>{value.modalStore.closeModal()}}>X</button>
         <div className={styles.loginSection}>
-          <button 
-            className={`${styles.google} ${styles.loginButtons}`} 
-            onClick={()=>{value.modalStore.closeModal(); value.authStore.googleLogin(); }}>Google</button>
-          <button 
-            className={`${styles.github} ${styles.loginButtons}`}
-            onClick={()=>{value.modalStore.closeModal(); value.authStore.githubLogin(); }}>Github</button>
-          <button 
-            className={`${styles.naver} ${styles.loginButtons}`}
-            onClick={()=>{value.modalStore.closeModal(); value.authStore.naverLogin(); }}>Naver</button>
+          <Button variant="contained"
+          sx={{ bgcolor: theme.socialLogin.google, ":hover": {boxShadow: 6,}, my: 2, p: 2}} 
+          onClick={()=>{value.modalStore.closeModal(); value.authStore.googleLogin();}}>Google</Button>
+          <Button variant="contained" 
+          sx={{ bgcolor: theme.socialLogin.github, my: 2, p: 2}} 
+          onClick={()=>{value.modalStore.closeModal(); value.authStore.githubLogin(); }}>Github</Button>
+          <Button variant="contained" 
+          sx={{ bgcolor: theme.socialLogin.naver, my: 2, p: 2 }} 
+          onClick={()=>{value.modalStore.closeModal(); value.authStore.naverLogin(); }}>Naver</Button>
         </div>    
       </div>
     </section>
