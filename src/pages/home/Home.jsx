73489a97efe26966a67830198522697f7d428949
2cleanwater <2cleanwater@naver.com>
2023-01-04 17:25:07 +0900

    feat: create skeleton of Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import styles from './Home.module.css';
 import useStore from '../../store/useStore';
 import { observer } from 'mobx-react';
+import CardMini from '../../components/card/CardMini';
+import Wave from '../../components/wave/Wave';
+import { Box } from '@mui/material';
 
 const Home = () => {
   const {value} = useStore();
   return (
     <div>
-      Home
+      <div>
+        <div>image of wave</div>
+        <div>당신의 기록 어쩌구 저쩌구</div>
+      </div>
+      {value.authStore.user&&
+        (<div>
+          <Wave></Wave>
+          <Link to='/card'><button>파도 추가하기</button></Link>
+        </div>)
+      }
+      <Box sx={{display:'flex'}}>
+        <CardMini/>
+        <CardMini/>
+        <CardMini/>
+      </Box>
     </div>
   )
 }
