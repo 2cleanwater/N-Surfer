80594fecb799009f63a53303010c2d251bea4b97
2cleanwater <2cleanwater@naver.com>
2023-01-04 17:25:14 +0900

    feat: create skeleton of Profile.jsx

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import CardDetail from '../../components/card/CardDetail';
+import Wave from '../../components/wave/Wave';
+import useStore from '../../store/useStore'
+import { Link } from 'react-router-dom';
 
-function Profile() {
+const Profile = () => {
+  const {value} = useStore();
   return (
-    <div>Profile</div>
+    <div>
+      <div className='ProfilePic'>
+        <div>
+          이미지
+        </div>
+        <div>
+          <button>이미지 수정하기</button>
+          <button>탈퇴하기</button>
+        </div>
+      </div>
+
+      <div className='wave'>
+        <Wave></Wave>
+      </div>
+      <div>
+        <CardDetail></CardDetail>
+      </div>
+      <Link to='/card/list'><button>더보기</button></Link>
+    </div>
   )
 }
 
