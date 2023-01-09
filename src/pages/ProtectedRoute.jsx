7d595f95778c615638d6b47d420d5a4cb0827330
2cleanwater <2cleanwater@naver.com>
2023-01-09 22:41:14 +0900

    feat: add protecedroute for kakao

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -4,10 +4,11 @@ import { observer } from 'mobx-react';
 import useStore from '../store/useStore';
 
 const ProtectedRoute = ({children})=>{
-  const {value} = useStore();
-  if(!value.authStore.user){
+  // const {value} = useStore();
+  if(!localStorage.getItem('token')){
     return <Navigate to='/' replace />
   }
   return children;
 }
-export default observer(ProtectedRoute);
+// export default observer(ProtectedRoute);
+export default ProtectedRoute;
