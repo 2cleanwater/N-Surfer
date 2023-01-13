2d62379125e9c156cffd7dbb4ddb17b43876b214
2cleanwater <2cleanwater@naver.com>
2023-01-13 23:46:47 +0900

    feat: connect mobx store

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-function User() {
+import { observer } from 'mobx-react-lite';
+import useStore from '../../store/useStore';
+
+const User = () => {
+  const {value} = useStore();
   return (
     <div>User</div>
   )
 }
 
-export default User
+export default observer(User);
