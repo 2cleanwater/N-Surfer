d91ca30fba8114a28edf439d8d3654606cf2cada
2cleanwater <2cleanwater@naver.com>
2023-02-03 23:44:26 +0900

    Design: set css on profile

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,21 +1,20 @@
-import React from 'react'
 import CardDetail from '../../components/card/CardDetail';
 import Wave from '../../components/wave/Wave';
-import UserProfile from '../../components/UserProfile';
+import UserProfile from '../../components/userProfile/UserProfile';
 import { Link } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 
 const Profile = () => {
   return (
-    <div>
+    <Box sx={{display:"flex"}}>
       <UserProfile/>
-      <Box>
-        <Wave></Wave>
+      <Box sx={{border:"1px solid", width:"500px"}}>
+        <Wave/>
+        <CardDetail/>
+        <Link to='/card/list'><button>더보기</button></Link>
       </Box>
-      <CardDetail/>
-      <Link to='/card/list'><button>더보기</button></Link>
-    </div>
+    </Box>
   )
 }
 
