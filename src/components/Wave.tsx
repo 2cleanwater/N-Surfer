5670c64c14dec14d07e673527bb4a05acdaa7c25
2cleanwater <2cleanwater@naver.com>
2023-03-29 14:10:45 +0900

    feat: add  arrows to change date& tooltip

--- src/components/Wave.tsx
+++ src/components/Wave.tsx
@@ -1,16 +1,15 @@
-import { Box, styled } from '@mui/material';
+import { Box, IconButton, styled, Tooltip } from '@mui/material';
 import { observer } from 'mobx-react'
 
 import { useRootStore } from '@provider/rootContext';
 import WaveBox from '@components/WaveBox';
-import instance from '@/service/axiosInterceptor';
-import { wave } from '@/store/WaveStore';
-import { color, display, fontSize, fontWeight, width } from '@mui/system';
+import { waveData } from '@/store/WaveStore';
 import { useEffect, useRef, useState } from 'react';
 
-//테스트 파일
-// import testJson from '@'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
+// css
 const DayList = styled("li")({
   flexGrow: "1",
   display: "inline-block",
@@ -18,41 +17,66 @@ const DayList = styled("li")({
   lineHeight: "60px"
 });
 
+// interface
+interface hoverDate {
+  date: string,
+  number: number
+}
+
 const Wave = ({userName}:{userName:string}) => {
   const value = useRootStore()!;
-  const [hoverData, setHoverData]= useState(["", 0, 70]);
+  const waveStore = value.waveStore;
+  const latestDate = waveStore.calFirstDate(new Date());
+  const [firstDate,setFirstDate] = useState<Date>(latestDate);
+  const [stringDate, setStringDate]= useState<string>(waveStore.stringifyDate(firstDate));
+  // date, date의 wave 갯수, index
+  // const [hoverData, setHoverData]= useState<Array<string,number,number>>(["", 0, 70]);
+  const [hoverData, setHoverData]= useState<hoverDate>({date:"",number:0});
+  const [hoverIndex, setHoverIndex]= useState<number>(70);
   const [waveToggle, setWaveToggle]= useState(false);
+  const [waveList, setWaveList]= useState<Array<waveData>>([]);
 
-  // userData 받아오기 ==================================================
-  const getUserWave= async function(){
-    const userWaveUrl = `/user/wave?userName=${userName}`;
-    await instance({
-      method: "GET",
-      url: userWaveUrl,
-      headers:{
-        'Content-Type': 'application/json'
-      }})
-      .then((res)=>{
-        return res.data;
-      })
-      .catch((err)=>{
-        console.log(err);
-        window.alert(userName + "의 Wave를 가져올 수 없습니다.");
-      })
-  }
-  // const userWaveList = getUserWave();
+  // server data ==============================================
+  // 날짜가 바뀔 때마다 웨이브를 불러오기 
+  useEffect(()=>{
+    // waveStore.getWaveList(userName,10,stringDate,setWaveList);
+    console.log((firstDate))
+  },[stringDate])
 
   // Test data ================================================
-  const userWaveList= require("@test/waveData.json");
+  useEffect(()=>{
+    setWaveList(require("@test/waveData.json") as Array<waveData>);
+  },[])
 
-  const userWaveForm= value.waveStore.matchWaveForm(userWaveList as Array<wave>);
-  console.log(hoverData);
+  const userWaveForm= waveStore.matchWaveForm(waveList);
 
   //Click 체크
   const isClicked=(index:number)=>{
-    if(index===hoverData[2]){
+    // if(index===hoverData[2]){
+    if(index===hoverIndex){
       return true
     }else return false
+  }
+  
+  // 날짜 동일 확인
+  const isSameDay = (target1:Date, target2: Date) => {
+    return waveStore.stringifyDate(target1) === waveStore.stringifyDate(target2)
+  }
+
+  // 날짜 변경
+  const changeDate= (date:Date, number:number)=>{
+    date.setDate(date.getDate()+ number);
+    setFirstDate(date);
+    setStringDate(waveStore.stringifyDate(date))
+    // console.log(firstDate)
+  }
+
+  // 날짜 표현
+  function formatDate(dateString:string) {
+    const year = dateString.slice(0, 4);
+    const month = dateString.slice(4, 6);
+    const day = dateString.slice(6, 8);
+    return `${year}년 ${month}월 ${day}일`;
   }
 
   return (
@@ -61,20 +85,20 @@ const Wave = ({userName}:{userName:string}) => {
       backgroundColor:"waveBackground",
       justifyContent:"center",
       alignContent: "center",
-      width: "800px",
-      height: "100%",
+      width: "900px",
       borderRadius:"2em",
       paddingTop:"20px",
-      paddingBottom:"40px"
+      paddingBottom:"40px",
+      position:"relative"
       }}>
       <Box sx={{display:"flex", justifyContent: "space-between"}}>
         <Box sx={{textAlign:"left", color:"white", paddingLeft:"40px",paddingBottom:"20px", fontSize:"2em", fontWeight:"Bold", textShadow:"2px 2px 2px gray"}}>
           "{userName}"님의 파도</Box>
-        <Box sx={{width:"170px", height:"30px", display:"flex", flexDirection:"row", justifyContent: "space-between", alignItems: "center", padding:"10px", marginRight:"40px", backgroundColor:"#F2E0C9",borderRadius: "30px",boxShadow: 3}}>
-          {hoverData[0]?
-          (<Box sx={{fontWeight:"bold", color:"#0F8DBF", paddingLeft:"10px"}}>{hoverData[0]}: {hoverData[1]}번</Box>):
+        <Box id="singleItemValue" sx={{width:"220px", height:"30px", display:"flex", flexDirection:"row", justifyContent: "space-between", alignItems: "center", padding:"10px", marginRight:"40px", backgroundColor:"#F2E0C9",borderRadius: "30px",boxShadow: 3}}>
+          {hoverData.date?
+          (<Box sx={{fontWeight:"bold", color:"#0F8DBF", paddingLeft:"10px"}}>{formatDate(hoverData.date)} : {hoverData.number}번</Box>):
           (<div></div>)}
-          <Box component="img" 
+          <Box component="img" id="waveToggle"
           sx={{width:"30px", height:"30px", justifyContent:"center",alignContent: "center",borderRadius:"2em", backgroundColor: waveToggle?"DarkBlue":"White", 
           "&:hover": {
             transform: "scale(1.3)"
@@ -98,14 +122,22 @@ const Wave = ({userName}:{userName:string}) => {
           <div key={rowIndex} style={{ display: "flex",flexDirection:"column"}}>
             {[...Array(7)].map((_, colIndex) => {
               const index = rowIndex*7 + colIndex;
-              let item:wave={ date: '', count: 0 };
+              let item:waveData={ date: '', count: 0 };
               Array.from(userWaveForm).map(([date, count], mapIndex) => {
                 if (mapIndex === index) item = { date, count };
               });
               return (
-                <div key={`${colIndex}-${rowIndex}`} style={{ width: "100%"}} onClick={()=>{hoverData[2]==index?setHoverData(["", 0, 70]):setHoverData([item.date,item.count,index])}}>
+                <div key={`${colIndex}-${rowIndex}`} style={{ width: "100%"}} onClick={()=>{
+                  if(hoverIndex===index) {
+                    setHoverData({date:"",number:0});
+                    setHoverIndex(70);
+                  } else {
+                    setHoverData({date:item.date,number:item.count});
+                    setHoverIndex(index);
+                  }
+                }}>
                   {item.date ? (
-                    <WaveBox date={item.date} count={item.count} toggle={waveToggle} isClicked={isClicked(index)}/>
+                      <WaveBox date={item.date} count={item.count} toggle={waveToggle} isClicked={isClicked(index)}/>
                   ) : null}
                 </div>
               );
@@ -114,6 +146,22 @@ const Wave = ({userName}:{userName:string}) => {
         ))}
         </Box>
       </Box>
+      <IconButton type="submit" size="small" sx={{position:"absolute", bottom:"7%", left:"2em", color: "white", backgroundColor:"#0F7B6C", 
+          "&:hover":{
+            color: "#0F7B6C", backgroundColor:"white",
+            transform: "scale(1.1)",
+            cursor : "pointer"
+          }}} onClick={(e)=>{changeDate(firstDate, -70)}}>
+          <ChevronLeftIcon fontSize="small" />
+        </IconButton>
+        <IconButton type="submit" size="small" disabled={isSameDay(firstDate,latestDate)?true:false} sx={{position:"absolute", bottom:"7%",right:"2em", color: "white", backgroundColor:"#0F7B6C", 
+          "&:hover":{
+            color: "#0F7B6C", backgroundColor:"white",
+            transform: "scale(1.1)",
+            cursor : "pointer"
+          }}} onClick={(e)=>{changeDate(firstDate, +70)}}>
+          <ChevronRightIcon fontSize="small" />
+        </IconButton>
     </Box>
   )
 }
