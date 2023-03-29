2b2a43b18815c3e0a30f7016cea5a768a4ea7cdc
2cleanwater <2cleanwater@naver.com>
2023-03-29 14:04:47 +0900

    feat: add card mini

--- src/components/CardMini.tsx
+++ src/components/CardMini.tsx
@@ -1,21 +1,55 @@
 import { Box } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+import InteractiveWave from '@/components/InteractiveWave';
+import { OceanData } from '@/store/OceanStore';
+import { useRootStore } from '@/provider/rootContext';
 
-interface cardMiniPropsType{
-  cardId: string;
-}
-
-const CardMini = ({cardId}:cardMiniPropsType) => {
+const CardMini = ({OceanData}:{OceanData:OceanData}) => {
+  // css
+  const mainBox = {
+    width: 230, 
+    height: 300, 
+    boxShadow: 3, 
+    m:3, 
+    borderRadius:"1em", 
+    ":hover": {transform: "scale(1.02)", cursor:"pointer"},
+    justifyItems:"center",alignItems:"center", alignContent:"center", justifyContents:"center",
+    position:"relative",
+    textAlign:"center"
+  }
+  
+  const value = useRootStore();
+  const transData = value!.oceanStore.transDate;
   const navigate = useNavigate();
+  const baseOceanImg:string= process.env.REACT_APP_OCEAN_BASE_IMG!;
+  
   return (
-    <Box
-      sx={{border:"solid 1px", width: 280, height: 400, boxShadow: 3, m:2, p:1, borderRadius:"0.5em", ":hover": {transform: "scale(1.02)"}}}
-      onClick={()=>{navigate(`/card/${cardId}`)}}
-    >
-      <h2>{cardId}의 제목입니다</h2>
-      <Box component="img" src={require('@static/testImages/testCat.jpg')}
-      sx={{width: 230, height: 200, objectFit: 'cover'}} alt='CardImg'></Box>
-      <Box>고양이는정말귀엽습니다고양이는뒷목을잡으면야옹하고웁니다야옹야옹아무것도안한고양이는꼭저를보는것같습니다찔리는마음입니다.</Box>
+    <Box sx={mainBox} onClick={()=>{navigate(`/card/${OceanData.cardId}`)}}> 
+      <Box sx={{position:"absolute", zIndex:"-1"}}> 
+        <InteractiveWave width={230} height={300} color="#E7BD73"></InteractiveWave>
+      </Box>
+      <Box sx={{fontSize:"20px",fontWeight:"700",padding:"20px", paddingBottom:"10px", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis"}}>
+        {OceanData.title}</Box>
+      <Box component="img" src={OceanData.images[0]?.imageUrl||baseOceanImg}
+      sx={{minWidth: 180, minHeight: 120, width: 180, height: 120, objectFit: 'cover', justifyItems:"center",alignItems:"center", alignContent:"center", justifyContents:"center", borderRadius:"1em", boxShadow: 3}} alt='CardImg'></Box>
+      <Box sx={{display:"flex", justifyContent: "space-between", paddingLeft:"25px", paddingRight:"25px", paddingTop:"10px"}}>
+        <Box sx={{width:"110px",fontSize:"13px", textAlign:"left", fontWeight:"300"}}>{transData(OceanData?.createDate ?? "")}</Box>
+        <Box sx={{width:"110px",fontSize:"13px", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis", fontWeight:"400", textAlign:"right"}}>
+          {OceanData.username}</Box>
+      </Box>
+      <Box sx={{ padding:"20px",
+        paddingTop:"10px",
+        paddingBottom: "30px",
+        overflow: 'hidden',
+        display: '-webkit-box',
+        WebkitLineClamp : "1",
+        WebkitBoxOrient: 'vertical',
+        wordWrap: "break-word",
+        mask:"linear-gradient(black, black, transparent)"}}>{OceanData.content}</Box>
+      {[...Array(10)].map((_, index) => (<div key={index}>
+        {OceanData.labels[index]&&<Box sx={{width:"15px",height:"10px",position:"absolute",left:"0%",
+        backgroundColor: OceanData.labels[index]?.color||"",top:`${(index+1) * 6}%`}}/>}
+      </div>))}
     </Box>
   )
 }
