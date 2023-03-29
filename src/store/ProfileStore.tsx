df53edde6f160bd64b39ceb27a78e07d7775aa98
2cleanwater <2cleanwater@naver.com>
2023-03-29 14:07:20 +0900

    refactor: change profile Store

--- src/store/ProfileStore.tsx
+++ src/store/ProfileStore.tsx
@@ -1,20 +1,20 @@
 import instance from '@service/axiosInterceptor';
 import AuthStore from './AuthStore';
 
-export interface UserData{
+export interface UserDataForm{
   useId?: number;
   userEmail?: string;
   provider?: string;
   userName?: string;
   userBirth?: string;
-  userType?: string;
   imgUrl?: string;
-  isOwned?: boolean;
+  totalWave?: number;
+  todayWave?: number;
 }
 
-export interface ProfileData{
-  userData: UserData;
-  setMyUserData: (userData:UserData)=>void;
+export interface ProfileStoreForm{
+  userData: UserDataForm;
+  setMyUserData: (userData:UserDataForm)=>void;
   getMyUserData: ()=>void;
   patchMyUserData: (formData:FormData)=>void;
   deleteMyUserData: ()=>void;
@@ -22,10 +22,10 @@ export interface ProfileData{
 
 const profileUrl:string = "/my-page/profile";
 
-const ProfileStore = (): ProfileData => {
+const ProfileStore = (): ProfileStoreForm => {
   return {
     userData: {},
-    setMyUserData: function(userData: UserData){
+    setMyUserData: function(userData: UserDataForm){
       this.userData = JSON.parse(JSON.stringify(userData));
     },
     getMyUserData: async function(){
@@ -34,12 +34,9 @@ const ProfileStore = (): ProfileData => {
         url: profileUrl,
         headers: {
           'Content-Type': 'application/json'
-        }
-        })
+        }})
         .then((res)=>{
-          this.setMyUserData(res.data as UserData);
-          console.log(this.userData);
-          return res.data;
+          this.setMyUserData(res.data as UserDataForm);
         }).catch((err) => {
           console.log(err);
           window.alert("정보를 가져올 수 없습니다.");
@@ -54,12 +51,13 @@ const ProfileStore = (): ProfileData => {
         url: profileUrl,
         data: formData,
         headers: {
-          'Content-Type': 'multipart/form-data"'
+          'Content-Type': 'multipart/form-data'
         }
       })
       .then(((res)=>{
         console.log(res)
-        this.setMyUserData(res.data as UserData);
+        // this.setMyUserData(res.data as UserDataForm);
+        this.getMyUserData();
         window.alert("성공적으로 변경되었습니다.")
       }))
       .catch((err)=>{
