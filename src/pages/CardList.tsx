fb4298bedf848ffa2bfe6241b4bff7659b24756d
2cleanwater <2cleanwater@naver.com>
2023-04-06 10:08:54 +0900

    feat: adding intersectionObserver

--- src/pages/CardList.tsx
+++ src/pages/CardList.tsx
@@ -1,89 +1,122 @@
-import CardMini from '@/components/CardMini'
-import instance from '@/service/axiosInterceptor'
-import { OceanData, label, wholeLabelList } from '@/store/OceanStore'
-import { Box, Button, IconButton, TextField } from '@mui/material'
-import { useEffect, useRef, useState } from 'react'
-import { useNavigate, useSearchParams } from 'react-router-dom'
+import CardMini from '@components/CardMini'
+import instance from '@service/axiosInterceptor'
+import { OceanData, label, wholeLabelList } from '@store/OceanStore'
+import Loading from '@components/Loading'
+import { useRootStore } from '@provider/rootContext'
 
-import SearchIcon from '@mui/icons-material/Search';
+import { ReactEventHandler, useEffect, useRef, useState } from 'react'
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom'
 import { observer } from 'mobx-react'
-import Loading from '@/components/Loading'
 
+import { Box, Button, IconButton, TextField } from '@mui/material'
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import SearchIcon from '@mui/icons-material/Search';
+
+import useIntersectionObserver from '@service/useIntersectionObserver'
 
 const CardList =() => {
+  const value = useRootStore()!;
   const navigate = useNavigate();
+
+  const labelList:Array<string> = wholeLabelList.map(item => item.name);
   const [searchParams, setSearchParams] = useSearchParams();
-  const [nicknameParams, setNicknameParams] = useState(searchParams.get("nickname")||"");
-  const [labelParams, setLabelParams] = useState(searchParams.get("label")||"");
   const [oceanList, setOceanList] = useState<Array<OceanData>>([]);
+  const [inputNickname, setInputNickname] = useState<string>("");
+  const labelParams:string= searchParams.get("label")||"";
+  const nicknameParams:string= searchParams.get("nickname")||"";
+  const selectedIndex:number = labelParams?labelList.indexOf(labelParams):-1
+
+  const [cardExist, setCardExist]= useState<boolean>(true);
+
+  // 무한스크롤 파트
+  // const [isLoaded, setIsLoaded] = useState(false);
+  // const [itemIndex, setItemIndex] = useState(0);
+  // const [data, setData] = useState();
+  // // const [data, setData] = useState(CARD_DATA.slice(0, 10));
+  
+  // const testFetch = (delay = 1000) =>
+  //   new Promise((res) => {setTimeout(res, delay); console.log("헤이")});
+
+  // const getMoreItem = async () => {
+  //   setIsLoaded(true);
+  //   await testFetch();
+  //   setItemIndex((i) => i + 1);
+  //   // setData(data.concat(CARD_DATA.slice(itemIndex, itemIndex + 5)));
+  //   console.log(itemIndex)
+  //   console.log("검색")
+  //   setIsLoaded(false);
+  // };
+
+  // const onIntersect: IntersectionObserverCallback = async (
+  //   [entry],
+  //   observer
+  // ) => {
+  //   if (entry.isIntersecting && !isLoaded) {
+  //     observer.unobserve(entry.target);
+  //     await getMoreItem();
+  //     observer.observe(entry.target);
+  //   }
+  // };
+
+  // const { setTarget } = useIntersectionObserver({
+  //   root: null,
+  //   rootMargin: '0px',
+  //   threshold: 0.5,
+  //   onIntersect,
+  // });
+
 
   // ocean list 받아오기
-  const getOceanList= async function(){
-    const oceanListUrl = `/card?nickname=${nicknameParams}&label=${labelParams}`;
-    await instance({
-      method: "GET",
-      url: oceanListUrl,
-      headers:{
-        'Content-Type': 'application/json'
-      }
-    })
-    .then((res)=>{
-      setOceanList(res.data.cardList as Array<OceanData>|| [])
-    })
-    .catch((err)=>{
-      console.log(err);
-      window.alert("Ocean List의 정보를 가져올 수 없습니다.");
-    })
-  }
-
   // sever data =============================================
   useEffect(()=>{
-    setNicknameParams(searchParams.get("nickname")||"");
-    setLabelParams(searchParams.get("label")||"");
-    getOceanList();
+    //입력창과 파람을 비교해 파람기준으로 맞춰줌
+    if(nicknameParams!==inputNickname)setInputNickname(nicknameParams);
+    try{
+      setCardExist(true);
+      value.oceanStore.getOceanList(nicknameParams,labelParams,undefined,setOceanList);
+    }catch(err){
+      setCardExist(false);
+    }
   },[searchParams])
   
 
-  //label 선택
-  const labelList:Array<string> = wholeLabelList.map(item => item.name);
-  const [selectedIndex, setSelectedIndex] = useState<number>(labelList.indexOf(labelParams));
-
-  const tagOnClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    console.log((event.target as HTMLDivElement).textContent=="모두")
-    if (event.target != null) {
-      if((event.target as HTMLDivElement).textContent=="모두"){
-        navigate(`/card?nickname=${nicknameParams}&label=`);
-      }
-      else {
-        navigate(`/card?nickname=${nicknameParams}&label=${(event.target as HTMLDivElement).textContent}`)
-      }}
-  }
-
+  //label 선택 =============================================================
   const handleListItemClick = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-    index: number,
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
-    setSelectedIndex(index);
-    tagOnClick(event);
+      if((event.target as HTMLDivElement).textContent=="모두"){
+        nicknameParams?navigate(`/card?nickname=${nicknameParams}`):navigate(`/card`);
+      }
+      else{
+        nicknameParams?
+        navigate(`/card?nickname=${nicknameParams}&label=${(event.target as HTMLDivElement).textContent}`):
+        navigate(`/card?label=${(event.target as HTMLDivElement).textContent}`)
+      }
   };
 
-  // 회원이름 검색
+  // 회원이름 검색 =============================================================
   const searchOnClick = ()=>{
-    console.log(labelParams)
-    navigate(`/card?nickname=${nicknameParams}&label=${labelParams}`)
+    navigate(`/card?nickname=${inputNickname}&label=${labelParams}`)
+    navigate(`/card?nickname=${inputNickname}`)
   }
 
   const handleNicknameSearch= (e: React.ChangeEvent<HTMLInputElement>)=>{
-    setNicknameParams(e.target.value)
+    setInputNickname(e.target.value)
+  }
+
+  // 엔터로 검색 =============================================================
+  const activeEnter = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if(e.key === "Enter") {
+      searchOnClick();
+    }
   }
 
   return (
     <Box sx={{display:"flex", flexDirection:"column"}}>
       <Box id="title" sx={{ textAlign:"right" }}>
-        <TextField variant="outlined" label="회원이름 검색" value={nicknameParams} onChange={handleNicknameSearch} inputProps={{style: {backgroundColor:"white", borderRadius:"5px"}}}></TextField>
+        <TextField variant="outlined" label="회원이름 검색" value={inputNickname} onChange={handleNicknameSearch} onKeyDown={activeEnter} inputProps={{style: {backgroundColor:"white", borderRadius:"5px"}}}></TextField>
         <IconButton size="large" onClick={searchOnClick} >
           <SearchIcon fontSize="large" />
         </IconButton>
@@ -95,46 +128,47 @@ const CardList =() => {
             <List component="nav" aria-label="secondary mailbox folder">
               <ListItemButton
                 selected={selectedIndex === -1}
-                onClick={(event) => handleListItemClick(event, -1)}
+                onClick={(e) => handleListItemClick(e)}
               >
                 <ListItemText primary="모두"/>
               </ListItemButton>
               <ListItemButton
                 selected={selectedIndex === 0}
-                onClick={(event) => handleListItemClick(event, 0)}
+                onClick={(e) => handleListItemClick(e)}
               >
                 <ListItemText primary="프론트엔드" />
               </ListItemButton>
               <ListItemButton
                 selected={selectedIndex === 1}
-                onClick={(event) => handleListItemClick(event, 1)}
+                onClick={(e) => handleListItemClick(e)}
               >
                 <ListItemText primary="백엔드" />
               </ListItemButton>
               <ListItemButton
                 selected={selectedIndex === 2}
-                onClick={(event) => handleListItemClick(event, 2)}
+                onClick={(e) => handleListItemClick(e)}
               >
                 <ListItemText primary="상식" />
               </ListItemButton>
               <ListItemButton
                 selected={selectedIndex === 3}
-                onClick={(event) => handleListItemClick(event, 3)}
+                onClick={(e) => handleListItemClick(e)}
               >
                 <ListItemText primary="자료구조" />
               </ListItemButton>
               <ListItemButton
                 selected={selectedIndex === 4}
-                onClick={(event) => handleListItemClick(event, 4)}
+                onClick={(e) => handleListItemClick(e)}
               >
                 <ListItemText primary="알고리즘" />
               </ListItemButton>
             </List>
           </Box>
         </Box>
+
         {oceanList.length>0?
         (<Box sx={{display: "flex", flexDirection:"column",  pr:"20px", mb:"50px" }}>
-          {[...Array(100)].map((_, rowIndex) => (
+          {[...Array(3)].map((_, rowIndex) => (
             <div key={rowIndex} style={{ display: "flex",flexDirection:"row"}}>
               {[...Array(3)].map((_, colIndex) => {
                 const index = rowIndex*3 + colIndex;
@@ -146,10 +180,25 @@ const CardList =() => {
               })}
             </div>
           ))}
+          {/* <div ref={setTarget}>{isLoaded && <Loading/>}</div> */}
         </Box>):
-        (<Loading/>)}
+      //   (<Box sx={{display: "flex", flexDirection:"column",  pr:"20px", mb:"50px" }}>
+      //   {[...Array(3)].map((_, rowIndex) => (
+      //     <div key={rowIndex} style={{ display: "flex",flexDirection:"row"}}>
+      //       {[...Array(3)].map((_, colIndex) => {
+      //         const index = rowIndex*3 + colIndex;
+      //         return (
+      //         <Box key={`${colIndex}-${rowIndex}`} sx={{ border:"1px solid black",width: "200px", height: "800px",}}>
+      //           헤이헤이
+      //         </Box>
+      //         );
+      //       })}
+      //     </div>
+      //   ))}
+      //   <div ref={setTarget}>{isLoaded && <Loading/>}</div>
+      // </Box>):
+        (<Box sx={{width:"100%"}}>{cardExist?<Loading/>:<Box>작성된 글이 없습니다. 첫 파도를 일으켜보세요!</Box>}</Box>)}
       </Box>
-      
     </Box>
   )
 }
