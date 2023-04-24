18cc72335a1d24ef6a9e8d0b048efe5cb6115adf
2cleanwater <2cleanwater@naver.com>
2023-04-25 07:13:29 +0900

    feat: add scroll up button

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import { Outlet } from 'react-router-dom';
 import { observer } from 'mobx-react';
 
-import Navbar from '@components/Navbar'
-import Modal from '@components/Modal';
+import Navbar from '@components/utils/Navbar'
+import Modal from '@components/utils/Modal';
 import { useRootStore } from '@provider/rootContext';
 
-import { Box } from '@mui/material';
-
+import UpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { Fab } from '@mui/material';
 
 const App = ()=>{
   const value = useRootStore()!;
+  const handleTopButtonClick = () => {
+    if (!window.scrollY) return;
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });  
+  };
   return (
     <>
       <Navbar />
       <Outlet />
       {value.modalStore._IsModalOpen&&<Modal/>}
+      <Fab sx={{position:"fixed", right:"3em", bottom:"3em"}} size="medium" color="inherit" aria-label="add">
+        <UpIcon onClick={handleTopButtonClick} />
+      </Fab>
     </>
   )
 }
