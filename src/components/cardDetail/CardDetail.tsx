c66c6042da0cfc89c45b971cbf54587039972ebb
2cleanwater <2cleanwater@naver.com>
2023-04-29 01:54:37 +0900

    feat: add like, bookmark, views, profile img spot

--- src/components/cardDetail/CardDetail.tsx
+++ src/components/cardDetail/CardDetail.tsx
@@ -8,16 +8,27 @@ import { useEffect } from 'react';
 import { Box } from '@mui/material'
 import { useNavigate } from 'react-router-dom';
 
+import VisibilityIcon from '@mui/icons-material/Visibility';
+
 const CardDetail = ({oceanData}:{oceanData:OceanData}) => {
   const value = useRootStore();
   const navigate = useNavigate();
+  const profileBaseImg:string= process.env.REACT_APP_PROFILE_BASE_IMG!;
 
   return (
     <Box sx={{bgcolor:"waveBackground", width:"57em", alignItems:"center",borderRadius:"2em", p:"0.5em", mb:"3em",boxShadow: "5"}}>
       <Box sx={{bgcolor:"#2158A8", borderRadius:"1em",width:"25em", p:"0.8em", py:"1.5em",wordBreak:"break-all",m: "1em auto", position:"relative", fontWeight:"bolder", fontSize:"30px", color: "white", display:"flex", flexDirection:"column" ,justifyContent:"center", alignItems:"center", boxShadow: "5"}}>
         {oceanData.title}
-        <Box onClick={()=>{oceanData?.nickname&&navigate(`/user/profile?nickname=${oceanData.nickname}`)}} sx={{pr:"2em", color:"lightblue",fontWeight:"bold",fontSize:"20px" ,textAlign:"right", position:"absolute", bottom:"1em", right:"0","&:hover":{cursor:"pointer", scale:"1.1"}}}>
-          by. {oceanData?.nickname ?? "Unknown"}</Box>
+        <Box sx={{pl:"1em", color:"lightblue",fontWeight:"bold",fontSize:"20px" ,textAlign:"right", position:"absolute", bottom:"1em", left:"0", display:"flex", alignItems:"center"}}>
+          <VisibilityIcon sx={{mr:"0.5em"}}/> 12,345
+          {/* {oceanData?.nickname.toLocaleString('ko-KR') ?? "Unknown"} */}
+          </Box>
+        <Box onClick={()=>{oceanData?.nickname&&navigate(`/user/profile?nickname=${oceanData.nickname}`)}} sx={{pr:"1em", color:"lightblue",fontWeight:"bold",fontSize:"20px" ,textAlign:"right", position:"absolute", bottom:"1em", right:"0",display:"flex", alignItems:"center","&:hover":{cursor:"pointer", scale:"1.05"}}}>
+          {oceanData?.nickname ?? "Unknown"}
+          <Box component="img" sx={{ml:"0.5em",width: "1.5em", height: "1.5em", objectFit:"cover",objectPosition:"center" ,borderRadius: "50%"}} alt="profile"
+          src={profileBaseImg} />
+          {/* src={oceanData.userProfileImg?oceanData.userProfileImg:profileBaseImg} /> */}
+        </Box>
       </Box>
       <Box sx={{width:"50em", height:"5em", bgcolor:"#2E88C7", borderRadius:"1em",display:"flex", m: "0px auto", mb:"1em", justifyContent: "space-between", alignItems:"center", boxShadow: "5"}}>
         {oceanData.labels&&<Box sx={{display:"flex"}}>
