309a71c0e80c422fedba270960ad32491800a412
2cleanwater <2cleanwater@naver.com>
2024-03-28 03:39:12 +0900

    chore: add text on switch & adjust margin

--- src/components/lottery/LotteryMachine.jsx
+++ src/components/lottery/LotteryMachine.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './LotteryMachine.css'
-import { Box, Modal, Typography, styled } from '@mui/material';
+import { Box, Modal, Typography, styled, Tooltip } from '@mui/material';
 
 import instance from '@service/axiosInterceptor'
 import { dateConverter } from '@service/dateConverter';
@@ -174,11 +174,25 @@ const LotteryMachine = () => {
         </div>
 
         <div className="gachapon">
-          <svg className="switch" viewBox="0 0 154 155" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={()=>{clickSwitch();}}>
-            <circle cx="76.5828" cy="77.7004" r="43.5" transform="rotate(-45 76.5828 77.7004)" fill="#C6D2D5" stroke="#57162F" strokeWidth="10" />
-            <path d="M32.7422 110.934C30.399 108.591 30.399 104.792 32.7422 102.449L101.732 33.4592C104.075 31.1161 107.874 31.1161 110.217 33.4592L120.117 43.3587C122.46 45.7018 122.46 49.5008 120.117 51.844L51.127 120.834C48.7838 123.177 44.9848 123.177 42.6417 120.834L32.7422 110.934Z" fill="#C6D2D5" stroke="#57162F" strokeWidth="10" />
-          </svg>
-
+          <Tooltip 
+            title={<div style={{fontSize:"15px"}}>뽑아보세요!</div>} placement="right" arrow
+            slotProps={{
+              popper: {
+                modifiers: [
+                  {
+                    name: 'offset',
+                    options: {
+                      offset: [0, -14],
+                    },
+                  },
+                ],
+              },}}>
+            <svg className="switch" viewBox="0 0 154 155" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={()=>{clickSwitch();}}>
+              <circle cx="76.5828" cy="77.7004" r="43.5" transform="rotate(-45 76.5828 77.7004)" fill="#C6D2D5" stroke="#57162F" strokeWidth="10" />
+              <path d="M32.7422 110.934C30.399 108.591 30.399 104.792 32.7422 102.449L101.732 33.4592C104.075 31.1161 107.874 31.1161 110.217 33.4592L120.117 43.3587C122.46 45.7018 122.46 49.5008 120.117 51.844L51.127 120.834C48.7838 123.177 44.9848 123.177 42.6417 120.834L32.7422 110.934Z" fill="#C6D2D5" stroke="#57162F" strokeWidth="10" />
+            </svg>
+          </Tooltip>
+          
           <svg className="machine" viewBox="0 0 617 979" fill="none" xmlns="http://www.w3.org/2000/svg">
 
             <circle cx="308.5" cy="388.5" r="302" fill="#DEECF3" stroke="#5A1830" strokeWidth="13" />
